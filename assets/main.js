343a0487145d24391400a04dac4ace8122cdec5d
AdilTehranli <adiljt@code.edu.az>
2023-07-02 18:08:13 +0400

    Updated default elements

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,5 @@
-$('.all_cart_slider').slick({
+$(document).ready(function() {
+  $('.all_cart_slider').slick({
     
     infinite: false,
     speed: 300,
@@ -31,9 +32,9 @@ $('.all_cart_slider').slick({
 
     ]
   });
-
+  
   $('.logo_slider').slick({
-
+  
     speed: 300,
     slidesToShow: 6,
     slidesToScroll: 6,
@@ -61,42 +62,70 @@ $('.all_cart_slider').slick({
           slidesToScroll: 1
         }
       }
-
+  
     ]
   });
 
-$('.trading_slider').slick({
-  infinite: false,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 5,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
+  $('.trading_slider').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 5,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
       }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
+    ]
+  });
+
+  $('.home').mouseover(function(){
+    $('.dropdown-home').toggleClass('active')
+  })
+
 });
 
-$('.home').mouseover(function(){
-  $('.dropdown-home').toggleClass('active')
-})
+document.addEventListener("DOMContentLoaded", function() {
+  const clickImages = document.querySelectorAll(".click_img img");
+  const informationImage = document.getElementById("selected_image");
+
+  clickImages.forEach(function(image) {
+    image.addEventListener("click", function() {
+      const imagePath = this.getAttribute("src");
+      informationImage.setAttribute("src", imagePath);
+    });
+  });
+});
+
+let count = 0;
+const countElement = document.getElementById("count");
+document.getElementById("decrease").onclick = function() {
+    if (count > 0) {
+        count -= 1;
+        countElement.innerHTML = count;
+    }
+};
+document.getElementById("increase").onclick = function() {
+    count += 1;
+    countElement.innerHTML = count;
+};
+
